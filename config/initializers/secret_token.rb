49f00d5ced774e7e362aab9c88b2ae1032f780aa
Alex Leach <aleach84@gmail.com>
2014-01-16 23:22:25 -0600

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chicagoartbeat::Application.config.secret_key_base = 'b22aa7b92545103e6f8790e41272f5aa3016f1743711cfdf1d6ffca1f19c0dd7d3f14790fa4a4851af2f54e3f9f04c86d17a9c1a90bb9bdf3037214fd9b7f465'
